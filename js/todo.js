97907a01cad136ace50a688eedfc5c82aed27819
radwan mohammed <mohammedfradwan@gmail.com>
2018-07-11 19:15:20 +0200

    Step 2 Set the counter for undone tasks

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,68 @@
-// Your code goes here!
+let TODOS = [];
+
+function update() {
+  const $todoList = document.querySelector(".todo-list");
+  const $todoCount = document.querySelector('.todo-count');
+  $todoList.innerHTML = "";
+  for (const item of TODOS) {
+    const $li = document.createElement("li");
+    if (item.done) {
+      $li.classList.add("completed");
+    }
+    $todoList.appendChild($li);
+
+    // Toggle button
+    const $toggle = document.createElement("input");
+    $toggle.className = "toggle";
+    $toggle.setAttribute("type", "checkbox");
+    if (item.done) {
+      $toggle.setAttribute("checked", "checked");
+    }
+    $toggle.addEventListener("change", onToggleTodo.bind(null, item.id));
+    $li.appendChild($toggle);
+
+    // Label
+    const $label = document.createElement("label");
+    $label.innerHTML = item.title;
+    $li.appendChild($label);
+
+    // Delete button
+    const $button = document.createElement("button");
+    $button.className = "destroy";
+    $li.appendChild($button);
+  }
+
+  //set the counter
+  const $count = TODOS.filter(todo=> todo.done === false).length;
+  if ($count === 1)
+  $todoCount.innerHTML=$count +' item left' ;
+  else
+  $todoCount.innerHTML=$count +' items left' ;
+
+  document.querySelector(".main").style.display = "block";
+}
+
+function onToggleTodo(id) {
+  const todo = TODOS.find(todo => todo.id === id);
+  // TODOS.find(function(todo) { return todo.id === id; });
+  todo.done = !todo.done;
+  update();
+}
+
+function onNewTodo(e) {
+  const title = e.target.value;
+  // Same as this line:
+  //const title = document.querySelector(".new-todo").value;
+  console.log(title);
+  TODOS.push({
+    id: Date.now(),
+    title,
+    done: false
+  });
+  update();
+  e.target.value = "";
+}
+
+// Select the new todo input field.
+const $newTodo = document.querySelector(".new-todo");
+$newTodo.addEventListener("change", onNewTodo); 
